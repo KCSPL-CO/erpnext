af8ebe0e938ec24c263598534990bb18b00fbdb6
MegharaniD <megharani.devadiga@weassist.co.in>
2025-09-15 15:45:11 +0530

    Warehouse list api

--- erpnext/stock/report/stock_balance/Stock_balance_api.py
+++ erpnext/stock/report/stock_balance/Stock_balance_api.py
@@ -1,7 +1,34 @@
 import frappe
 from frappe import _
 from frappe.utils import nowdate
+import base64
+
  
+ # Authentication helper
+def authenticate_user():
+	auth_header = frappe.get_request_header("Authorization")
+	if not auth_header or not auth_header.startswith("Basic "):
+		frappe.local.response["http_status_code"] = 401
+		return None
+
+	try:
+		encoded_token = auth_header.split("Basic ")[1]
+		decoded = base64.b64decode(encoded_token).decode("utf-8")
+		api_key, api_secret = decoded.split(":")
+	except Exception:
+		frappe.local.response["http_status_code"] = 401
+		return None
+
+	user = frappe.db.get("User", {"api_key": api_key})
+	if (
+		not user
+		or frappe.utils.password.get_decrypted_password("User", user.name, "api_secret") != api_secret
+	):
+		frappe.local.response["http_status_code"] = 401
+		return None
+
+	return user
+
 @frappe.whitelist(allow_guest=False)
 def get_items_by_warehouse():
     """
@@ -53,4 +80,27 @@ def get_items_by_warehouse():
         "data": stock_data
     }
  
- 
+@frappe.whitelist(allow_guest=True)
+def listWarehouse():
+    if frappe.request.method != "GET":
+        frappe.local.response["http_status_code"] = 405
+        return {"error": "Only GET method allowed"}
+
+    if not authenticate_user():
+        return {"error": "Unauthorized"}
+
+    try:
+        warehouse = frappe.get_all(
+            "Warehouse",
+            fields=["*"],
+            order_by="creation desc",
+        )
+        return {
+            "message": warehouse,
+           
+        }
+
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "List Warehouse")
+        frappe.local.response["http_status_code"] = 500
+        return {"error": str(e)}
